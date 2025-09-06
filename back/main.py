c3f9e0200970ad4e8354e5797b98f127d47c3907
Minchan Kim <kimmc3423@naver.com>
2025-09-07 07:22:32 +0900

    feat: CORS변경

--- back/main.py
+++ back/main.py
@@ -12,10 +12,10 @@ app = FastAPI(title="구름톤 FastAPI 백엔드")
 # CORS 미들웨어 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=[""],
-    allow_headers=[""],
+    allow_origins=["https://2025-seasonthon-team-6-fe.vercel.app/"],
+    allow_credentials=False,  # credentials를 False로
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
 )
 # 라우터 등록
 app.include_router(dpm_router.router)
