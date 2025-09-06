62cf4216da640fe96e57e4e2dbc71b2b91044623
Minchan Kim <kimmc3423@naver.com>
2025-09-07 07:06:01 +0900

    feat: 캘린더 라우터 추가

--- back/main.py
+++ back/main.py
@@ -3,17 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from app.routers import dpm_router
 from app.routers import auth_router
+from app.routers import calender_router
 
 load_dotenv()
 
 app = FastAPI(title="구름톤 FastAPI 백엔드")
 
 # CORS 미들웨어 추가
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"]  # 모든 오리진 허용
-)
+app.add_middleware(CORSMiddleware, allow_origins=["*"])  # 모든 오리진 허용
 
 # 라우터 등록
 app.include_router(dpm_router.router)
 app.include_router(auth_router.router)
+app.include_router(calender_router.router)
