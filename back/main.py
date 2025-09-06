7433c7b0bbffa7ed01267602056d3fb606878219
Minchan Kim <kimmc3423@naver.com>
2025-09-07 07:20:20 +0900

    feat: CORS변경

--- back/main.py
+++ back/main.py
@@ -10,8 +10,13 @@ load_dotenv()
 app = FastAPI(title="구름톤 FastAPI 백엔드")
 
 # CORS 미들웨어 추가
-app.add_middleware(CORSMiddleware, allow_origins=["*"])  # 모든 오리진 허용
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 # 라우터 등록
 app.include_router(dpm_router.router)
 app.include_router(auth_router.router)
